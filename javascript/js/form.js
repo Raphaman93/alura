a9ca45b9412ab782d5bf0b12354ccfda26574752
Raphael Manfre Silva- Estratégia Digital - Editora Globo <rmanfre@edglobo.com.br>
2018-01-11 17:57:07 -0200

    Aula 7 - removendo pacientes da tabela

--- javascript/js/form.js
+++ javascript/js/form.js
@@ -3,16 +3,39 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
 
-    var form = document.querySelector("#form-adiciona");
     //Extraindo informacoes do paciente do form
+    var form = document.querySelector("#form-adiciona");
+    
     var paciente = obtemPacienteDoFormulario(form);
      // Cria a tr e a td do paciente
-     var pacienteTr = montaTr(paciente);
+    var pacienteTr = montaTr(paciente);
+
+    var erros = validaPaciente(paciente);
+    console.log(erros);
+     if(erros.length > 0){
+        exibeMensagensdeErro(erros);
+        return;
+     }
+     
      var tabela = document.querySelector("#tabela-pacientes");
      tabela.appendChild(pacienteTr);
      
      form.reset();
-    
+     var mensagensErro = document.querySelector("#mensagens-erro");
+     mensagensErro.innerHTML = "";
+     
+});
+
+    function exibeMensagensdeErro(erros){
+        var ul = document.querySelector("#mensagens-erro");
+        ul.innerHTML = "";
+        erros.forEach(function(erro){
+            var li = document.createElement("li");
+            li.textContent = erro;
+            ul.appendChild(li);
+        });
+    }
+
     function obtemPacienteDoFormulario(form) {
 
         var paciente = {  
@@ -20,7 +43,7 @@ botaoAdicionar.addEventListener("click", function(event) {
             peso : form.peso.value,
             altura : form.altura.value,
             gordura : form.gordura.value,
-            imc: calculaImc(form.altura.value, form.peso.value)
+            imc: calculaImc(form.peso.value, form.altura.value)
         }
         return paciente
     }
@@ -34,7 +57,7 @@ botaoAdicionar.addEventListener("click", function(event) {
         pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
         pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
         pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
-        return pacienteTr;    
+        return pacienteTr;
     }
     
     function montaTd(dado,classe){
@@ -43,4 +66,18 @@ botaoAdicionar.addEventListener("click", function(event) {
         td.classList.add(classe);
         return td;
     }
-});
+
+    function validaPaciente(paciente){
+        var erros = [];
+        
+        if(paciente.nome.length == 0) erros.push("O nome não pode ficar em branco");
+        if(paciente.gordura.length == 0) erros.push ("A gordura não pode ser em branco");
+        if(paciente.peso.length == 0) erros.push ("O peso não pode ser em branco");
+        if(paciente.altura.length == 0) erros.push ("A altura não pode ser em branco");
+        if(!validaPeso(paciente.peso)) erros.push("Peso é inválido");
+        if(!validaAltura(paciente.altura)) erros.push("Altura é inválida");
+
+        return erros;
+    }
+
+
